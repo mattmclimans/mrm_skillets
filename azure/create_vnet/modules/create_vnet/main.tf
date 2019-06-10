8b6f18a1940703c51c6c6f84375d91b6ba5bc9ee
Matthew McLimans <mmclimans@Matthews-Mac-mini.local>
2019-06-10 19:34:45 -0400

    Update main.tf

--- azure/create_vnet/modules/create_vnet/main.tf
+++ azure/create_vnet/modules/create_vnet/main.tf
@@ -82,10 +82,9 @@ output "vnet_address_space" {
   description = "The address space of the newly created vNet"
   value       = "${azurerm_virtual_network.vnet.address_space}"
 }
-/*
+
 
 output "vnet_subnets" {
   description = "The ids of subnets created inside the newl vNet"
   value       = "${azurerm_subnet.subnet.*.id}"
 }
-*/
